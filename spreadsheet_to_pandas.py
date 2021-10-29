1d3d4f58860112a058f2e1e4c12823443eaf51ae
flaxattack <87672899+flaxattack@users.noreply.github.com>
2021-10-29 10:18:55 -0700

    Update spreadsheet_to_pandas.py

--- spreadsheet_to_pandas.py
+++ spreadsheet_to_pandas.py
@@ -14,7 +14,6 @@ creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FI
 
 
 # The ID and range of a sample spreadsheet.
-
 spreadsheet_id = 'spreadsheet_id'
 
 # Call the Sheets API
@@ -22,12 +21,12 @@ service = build('sheets', 'v4', credentials=creds)
 sheet = service.spreadsheets()
 result = sheet.get(spreadsheetId=spreadsheet_id).execute()
 
+# make a list of all the sheet names in the spreadsheet
+# this will allow you to get the data from each sheet
 titles = []
-#print(result['sheets'])
-for item in result['sheets']:	# access the list associated with "sheets"
-	for key, value in item.items():	# access the dictionary inside the list
-		# print(f"{value['title']}")
-		sheet_title = f"{value['title']}"	# find value for key 'title' in list of dictionaries
+for item in result['sheets']:	
+	for key, value in item.items():
+		sheet_title = f"{value['title']}"
 		title = sheet_title.replace('/', '_').replace(':', '')
 		titles.append(sheet_title)
 
@@ -38,24 +37,28 @@ for title in titles[]:
 	request = sheet.values().get(spreadsheetId=spreadsheet_id, range=title)
 	response = request.execute()
 	data = response.get('values')
-	names = [d.title() for d in data[0]]
-		# creates a (capitalized) list of names that is the first item in the list "data"
-	df = pd.DataFrame(data[1:], columns=names)
-		# 'data' is an array of values representing rows in the sheet
-		# data[0] is the first item in the array, i.e. the columns (['', 'name1', 'name2'])
-		# set the dataframe to the data array excluding the first item
-		# the first item is set to 'columns'
-	df[''] = df[''].str.capitalize()
-		# capitalizes the index names
-	indexed = df.set_index('')
-		# sets the index of the dataframe to the first actual column ('birds', 'bonus cards', etc)
-	indexed_dfs.append(indexed)
-		# appends the reindexed dataframe to a list
 	
+	# create a (capitalized) list of names that is the first item in the list "data"
+	names = [d.title() for d in data[0]]
+	
+	# 'data' is an array of values representing rows in the sheet
+	# data[0] is the first item in the array, i.e. the columns (['', 'name1', 'name2'])
+	# set the dataframe to the data array (excluding the first item)
+	# the first item is set to 'columns'
+	df = pd.DataFrame(data[1:], columns=names)
 
+	# capitalize the index names
+	df[''] = df[''].str.capitalize()
+		
+	# sets the index of the dataframe to the first actual column ('birds', 'bonus cards', etc)
+	df.set_index('', inplace=True)	
+	indexed_dfs.append(df)
+
+# concatenate into a single dataframe 	
 all_values = pd.concat(indexed_dfs, axis=1)
-#print(all_values)
+
+# convert values to numeric from string
 all_values = all_values.apply(pd.to_numeric)
-#print(all_values)
-#print(all_values.groupby(axis=1, level=0).mean())
+
+# save the new dataframe to a csv file
 #all_values.to_csv('oceania_bird_scores2a.csv')
